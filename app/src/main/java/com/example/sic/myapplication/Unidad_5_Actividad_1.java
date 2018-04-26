02274072bc29f94e5cf71939e0ad565b32f363b0
Jose Hugo Cuaya Contreras <caitos_02@live.com.mx>
2018-04-25 23:25:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sic/myapplication/Unidad_5_Actividad_1.java
@@ -0,0 +1,13 @@
+package com.example.sic.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Unidad_5_Actividad_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unidad_5__actividad_1);
+    }
+}
